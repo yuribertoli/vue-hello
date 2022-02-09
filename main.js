5490f63b53316d0a2f4dfb57e995765276125672
yuribertoli <yuribertoli@yahoo.it>
2022-02-09 15:27:22 +0100

    img

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const root = new Vue (
         el: "#root",
         data: {
             messaggio: "hello",
-            immagine: 
+            immagine: "https://picsum.photos/200/300"
         },
         methods: {
             
